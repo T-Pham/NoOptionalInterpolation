0ee87a5cb96bf66a20877967e0f028de0e751362
Thanh Pham <minhthanh@me.com>
2016-07-20 15:24:46 +0800

    Create a separate library project

---
+++ NoOptionalInterpolation/NoOptionalInterpolation.h
@@ -0,0 +1,19 @@
+//
+//  NoOptionalInterpolation.h
+//  NoOptionalInterpolation
+//
+//  Created by Thanh Pham on 7/20/16.
+//  Copyright © 2016 Thanh Pham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NoOptionalInterpolation.
+FOUNDATION_EXPORT double NoOptionalInterpolationVersionNumber;
+
+//! Project version string for NoOptionalInterpolation.
+FOUNDATION_EXPORT const unsigned char NoOptionalInterpolationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NoOptionalInterpolation/PublicHeader.h>
+
+
